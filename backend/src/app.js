5a62a6e6218801a04c3accca4812e316a235c08c
iamkaizen <kimsokhom654@gmail.com>
2025-10-26 01:30:37 +0700

    Finish: TaskList page logic backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 module.exports = app;
